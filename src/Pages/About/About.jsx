796f3d1cd1aaa9516b963476797dc2db232fd4a6
Kiran0507 <kiranraval550@gmail.com>
2024-10-16 18:03:50 +0530

    footer code done

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../../Comman/Navbar/Navbar";
 import Breadcrumb from "../../Comman/Breadcrumb/Breadcrumb";
+import footer from "../../Comman/footer/footer";
 
 const About = () => {
   return (
     <>
       <Navbar />
-      <Breadcrumb  title ="About"/>
+      <Breadcrumb  title ="About" color="white"/>
+      <footer/>
     </>
   );
 };
