c48c324df619dda52a13b80ec0f0a938bb0ad5f3
Kiran0507 <kiranraval550@gmail.com>
2024-10-22 17:47:29 +0530

    about page

--- src/Pages/Career/Career.jsx
+++ src/Pages/Career/Career.jsx
@@ -17,7 +17,7 @@ const Career = () => {
         button="VIEW JOB OPENING"
       />
       <div data-aos="fade-up" data-aos-duration="3000">
-        <div className="row py-4 text-center m-sm-3">
+        <div className="row py-4 text-md-center m-sm-3">
           <div className="col-md-12">
             <h2 className="cartitlefirst">Perks Of Joining</h2>
           </div>
@@ -82,10 +82,192 @@ const Career = () => {
         </div>
       </div>
       {/* open position  */}
-      <div className="row py-5 text-center m-sm-3">
+      <div className="container">
+        <div className="row">
+      <div className="row py-5 text-md-center m-sm-3">
         <div className="col-md-12 ">
           <h2 className="carposition">Open Positions</h2>
         </div>
+      </div>
+      <div
+        class="col-md-12 aos-init aos-animate"
+        data-aos="zoom-in"
+        data-aos-duration="1000"
+      >
+        <div class="job-card">
+          <div class="row">
+            <div class="col-md-8">
+              <h4>React.js </h4>
+            </div>
+            <div class="col-md-4 text-end">
+              <button
+                class="btn btn-danger rounded-pill"
+                data-title="React.js "
+                onclick="toggleDescription('job-description-0')"
+                fdprocessedid="5kmtl"
+              >
+                View More
+              </button>
+              <a
+                href="#"
+                class="btn btn-dark rounded-pill"
+                data-bs-toggle="modal"
+                data-bs-target="#applyModal"
+                data-position="React.js "
+              >
+                Apply Now
+              </a>
+            </div>
+          </div>
+          <div
+            id="job-description-0"
+            class="job-description mt-3"
+            style={{ display: "none" }}
+          >
+            <div class="row">
+              <div class="col-md-10">
+                <ul>
+                  <li>
+                    <p>
+                      <strong>Experience:</strong> 0-1 years
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Skills:</strong> Basic understanding of
+                      JavaScript, HTML, and CSS. Experience with React.js is a
+                      plus but not required. Eagerness to learn and grow in a
+                      fast-paced environment.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Qualification:</strong> Pursuing or recently
+                      completed a degree in Computer Science, Software
+                      Engineering, or a related field. Strong analytical and
+                      problem-solving skills. Excellent communication and
+                      teamwork abilities.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Responsibility:</strong> Develop and maintain
+                      user-facing features using React.js, Collaborate with
+                      back-end developers and designers to implement user
+                      interfaces, Write reusable and efficient code,
+                      Troubleshoot and debug issues, Stay updated with the
+                      latest trends in front-end development.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Mode:</strong> Full Time
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Status:</strong> Open
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      </div>
+
+      {/* second button */}
+      <div className="container">
+        <div className="row">
+        <div
+        class="col-md-12 aos-init aos-animate"
+        data-aos="zoom-in"
+        data-aos-duration="1000"
+      >
+        <div class="job-card">
+          <div class="row">
+            <div class="col-md-8">
+              <h4>React.js </h4>
+            </div>
+            <div class="col-md-4 text-end">
+              <button
+                class="btn btn-danger rounded-pill"
+                data-title="React.js "
+                onclick="toggleDescription('job-description-0')"
+                fdprocessedid="5kmtl"
+              >
+                View More
+              </button>
+              <a
+                href="#"
+                class="btn btn-dark rounded-pill"
+                data-bs-toggle="modal"
+                data-bs-target="#applyModal"
+                data-position="React.js "
+              >
+                Apply Now
+              </a>
+            </div>
+          </div>
+          <div
+            id="job-description-0"
+            class="job-description mt-3"
+            style={{ display: "none" }}
+          >
+            <div class="row">
+              <div class="col-md-10">
+                <ul>
+                  <li>
+                    <p>
+                      <strong>Experience:</strong> 0-1 years
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Skills:</strong> Basic understanding of
+                      JavaScript, HTML, and CSS. Experience with React.js is a
+                      plus but not required. Eagerness to learn and grow in a
+                      fast-paced environment.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Qualification:</strong> Pursuing or recently
+                      completed a degree in Computer Science, Software
+                      Engineering, or a related field. Strong analytical and
+                      problem-solving skills. Excellent communication and
+                      teamwork abilities.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Responsibility:</strong> Develop and maintain
+                      user-facing features using React.js, Collaborate with
+                      back-end developers and designers to implement user
+                      interfaces, Write reusable and efficient code,
+                      Troubleshoot and debug issues, Stay updated with the
+                      latest trends in front-end development.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Mode:</strong> Full Time
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Status:</strong> Open
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+         </div>
       </div>
     </>
   );
